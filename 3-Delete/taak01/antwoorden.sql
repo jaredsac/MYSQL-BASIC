a37bcda9257c74545bcb7703f4d5e86942861fb3
Jared Sackey <jared.sackey@student.rocva.nl>
2020-06-08 14:57:41 +0200

    klaar

--- 3-Delete/taak01/antwoorden.sql
+++ 3-Delete/taak01/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE from players WHERE name = 'david sylva' 
 -- Opdracht 2 
-
+DELETE from players WHERE club = 'willem II' 
 -- Opdracht 3
-
+DELETE from players WHERE club = 'fc barcelona' and nationality = 'belgium' 
 -- Opdracht 4
-
+DELETE FROM players WHERE age >= 38 
 -- Opdracht 5
+DELETE FROM players WHERE id = 167905 or id = 169595 
